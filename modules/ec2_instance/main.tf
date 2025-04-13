ed5d4b4d99a0981ae06d0d0f5dd93aecbf7e8f33
Stan Zhou <stanzhou@encompasscorporation.com>
2025-04-13 22:37:20 +1000

    Add count

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -16,7 +16,12 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "hello_world" {
+  count         = var.instance_count
   ami           = data.aws_ami.ubuntu.id
   subnet_id     = var.subnet_id
   instance_type = var.instance_type
+
+  tags = merge(var.tags, {
+    Name = "${var.name_prefix}-{count.index}"
+  })
 }
