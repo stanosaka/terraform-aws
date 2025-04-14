e19c1bfdebec0395f8c577cc09a274e43c25905a
Stan Zhou <stan.osaka@gmail.com>
2025-04-14 21:00:32 +1000

    Update

--- modules/ec2_instance/examples/main.tf
+++ modules/ec2_instance/examples/main.tf
@@ -11,10 +11,17 @@ data "aws_subnets" "default" {
 }
 
 module "test_instance" {
-  source    = "../"
-  subnet_id = data.aws_subnets.default.ids[0]
+  source      = "../"
+  subnet_id   = data.aws_subnets.default.ids[0]
+  count       = 5
+  name_prefix = "test-instance"
 }
 
-output "aws_instance_arn" {
-  value = module.test_instance.aws_instance_arn
+output "aws_instance_arns" {
+  # value = [for instance in module.test_instance : instance.aws_instance_arns]
+  value = module.test_instance[*].aws_instance_arns
+}
+
+output "aws_instance_ips" {
+  value = module.test_instance[*].aws_instance_ips
 }
--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -1,11 +1,16 @@
-output "aws_instance_arn" {
-  value = aws_instance.hello_world.arn
+output "aws_instance_arns" {
+  # value       = [for instance in aws_instance.hello_world : instance.arn]
+  value       = aws_instance.hello_world[*].arn
+  description = "List of ARNs for all created EC2 instances."
 }
 
-output "aws_instance_ip" {
-  value = aws_instance.hello_world.private_ip
+output "aws_instance_ips" {
+  # value       = [for instance in aws_instance.hello_world : instance.private_ip]
+  value       = aws_instance.hello_world[*].private_ip
+  description = "List of private IPs for all created EC2 instances."
 }
 
-output "aws_instance" {
-  value = aws_instance.hello_world
-}
+# output "aws_instances" {
+#   value       = aws_instance.hello_world
+#   description = "List of all created EC2 instances."
+# }
